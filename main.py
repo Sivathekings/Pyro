3dbbf3a0d8f4686251b9599531caec3bcd4f1e12
Sivathekings <sivathekingorg@gmail.com>
2024-05-22 08:31:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ Siva = Client(
     api_hash = "6dffe39cf4f3f43154619eb99b35b7db",
     bot_token = "6829475237:AAGl3867Iv_Hji268Z7ML9-VnfnFN77-Ue0",
     plugins=dict(root="Siva")
+)
+
+
+
+print("bot is started")
+Siva.run()
