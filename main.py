2b8aee146d5e2901401def340fc97ea29d42b396
Sivathekings <sivathekingorg@gmail.com>
2024-05-22 08:22:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 Siva = Client(
     "pyrogrambot",
-    api_id = "",
-    api_hash = "",
-    bot_token = "",
+    api_id = "9978416",
+    api_hash = "6dffe39cf4f3f43154619eb99b35b7db",
+    bot_token = "6829475237:AAGl3867Iv_Hji268Z7ML9-VnfnFN77-Ue0",
     plugins=dict(root="Siva")
