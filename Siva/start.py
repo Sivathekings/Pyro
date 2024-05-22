f18cb9ac9eecc9baed1b37870d5b035986e7c341
Sivathekings <sivathekingorg@gmail.com>
2024-05-22 08:35:01 +0530

    Update start.py

--- Siva/start.py
+++ Siva/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters 
 
-@Client.on_message(filters.commamd("start"))
+@Client.on_message(filters.command("start"))
 async def start_cmd(client, msg):
     await msg.reply_text("hello")
