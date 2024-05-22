1bf6da078aac5e8e5b7f2ee503b7abd67f8062bb
Sivathekings <sivathekingorg@gmail.com>
2024-05-22 08:48:55 +0530

    Update start.py

--- Siva/start.py
+++ Siva/start.py
@@ -1,5 +1,25 @@
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import asyncio
+import random
+
+
+PM_TEXT = f"""
+Hello {message.from_user.first_name} ⚡,
+I am Shiva robot...
+I am at construction. 
+"""
+
+START_BUTTON = [[
+    InlineKeyboardButton("👨 Owner 👨", url="https://t.me/siva_the_king")
+]]
+
 
 @Client.on_message(filters.command("start"))
 async def start_cmd(client, msg):
-    await msg.reply_text("hello")
+    await msg.reply_text(
+        text =  PM_TEXT,
+        reply_markup=InlineKeyboardMarkup(START_BUTTON)
+        
+    )
+
