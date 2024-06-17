8e50ab99a0aef4d84d3def5bacb9290b757e6282
Sivathekings <sivathekingorg@gmail.com>
2024-06-17 15:49:01 +0530

    Update __init__.py

--- Siva/__init__.py
+++ Siva/__init__.py
@@ -1,1 +1,21 @@
+from pyrogram import Client, filters
+import os 
+
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+TOKEN = os.getenv("TOKEN")
+
+Siva = Client(
+    "pyrogrambot",
+    api_id = "API_ID",
+    api_hash = "API_HASH",
+    bot_token = "TOKEN",
+    plugins=dict(root="Siva")
+)
+
+
+
+print("bot is started")
+Siva.run()
 
